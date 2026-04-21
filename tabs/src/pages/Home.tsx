6cd9507066d85842ce4d3bb6d17303ceba209c9d
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-21 14:48:09 -0400

    1.04

--- tabs/src/pages/Home.tsx
+++ tabs/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Tab4: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
