118475e6bfb33ab64902e1efba7fcd08e58ae2dd
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-28 14:40:14 -0400

    Update App.tsx

--- homessidebar/src/App.tsx
+++ homessidebar/src/App.tsx
@@ -3,7 +3,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import Page from './pages/Page';
-
+import Listings from './pages/HomeListPage';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -45,6 +45,9 @@ const App: React.FC = () => {
           <IonRouterOutlet id="main">
             <Route path="/" exact={true}>
               <Redirect to="/folder/Listings" />
+            </Route>
+            <Route path="/Listings" exact={true}>
+              <Redirect to="/listings" />
             </Route>
             <Route path="/folder/:name" exact={true}>
               <Page />
