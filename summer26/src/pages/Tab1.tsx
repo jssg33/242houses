380ab9529688f42f907c9c7a38c3fec128c1193c
jssg33 <john@glocation.info>
2026-05-25 17:39:54 -0400

    Update Tab1.tsx

--- summer26/src/pages/Tab1.tsx
+++ summer26/src/pages/Tab1.tsx
@@ -1,6 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
 import './Tab1.css';
+
+// Inline iframe component inside the same file
+const Project7Frame: React.FC = () => {
+  return (
+    <iframe
+      src="https://jssg33.github.io/usc242/project7/index.html"
+      title="Homes Frame"
+      style={{
+        width: "100%",
+        height: "100%",
+        border: "none"
+      }}
+      loading="lazy"
+    />
+  );
+};
 
 const Tab1: React.FC = () => {
   return (
@@ -10,16 +26,19 @@ const Tab1: React.FC = () => {
           <IonTitle>Homes</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Homes</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Homes" />
+
+        <Project7Frame />
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab1;
+
