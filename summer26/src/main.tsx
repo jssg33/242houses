44df6d4ea03e1db6d53edb3ea01834da1f604ae4
jssg33 <john@glocation.info>
2026-05-26 10:55:39 -0400

    Update main.tsx

--- summer26/src/main.tsx
+++ summer26/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/usc242/project/finalhomes/reactfinal">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
