bd2b05c1a09b1cf86770229c43e725e3bb843489
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-06-04 05:36:40 +0000

    V2.0

--- summer26/src/main.tsx
+++ summer26/src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
+
+
