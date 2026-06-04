b75d49976a8ceacf47db1d6edb2a71341dc7068b
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-06-04 05:50:11 +0000

    V4.0

---
+++ summer26/electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'info.glocation.houses',
+  appName: 'summer26',
+  webDir: 'dist'
+};
+
+export default config;
